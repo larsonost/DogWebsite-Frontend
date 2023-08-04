c4e31c0aed963ebc20e43459a56986a559aae8e7
Lenore8963 <zhouchen8963@gmail.com>
2023-08-04 01:20:51 -0700

    add simple waterfall effect to the tuits

--- src/tuits/index.js
+++ src/tuits/index.js
@@ -1,16 +1,18 @@
 import React from "react";
 import TuitItem from "./tuit-item";
-import {useSelector} from "react-redux";
+import { useSelector } from "react-redux";
+import "./waterfall.css";
+
 const TuitsList = () => {
-    const { tuits } = useSelector(state => state.tuits)
-    return (
-        <ul className="list-group">
-            {
-                tuits.map(tuit =>
-                    <TuitItem
-                        key={tuit._id} tuit={tuit} />)
-            }
-        </ul>
-    );
+  const { tuits } = useSelector((state) => state.tuits);
+
+  return (
+    <div className="waterfall">
+      {tuits.map((tuit) => (
+        <TuitItem key={tuit._id} tuit={tuit} />
+      ))}
+    </div>
+  );
 };
+
 export default TuitsList;
