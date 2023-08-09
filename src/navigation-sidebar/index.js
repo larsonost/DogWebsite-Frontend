f17720db3f95d453434b3d07a9448da9eb1f2c8c
Lenore8963 <zhouchen8963@gmail.com>
2023-08-08 18:01:24 -0700

    change some color

--- src/navigation-sidebar/index.js
+++ src/navigation-sidebar/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./nav.css";
 import { Link, useLocation } from "react-router-dom";
-import logo from "../images/logo.png";
+import logo from "../images/logo-new.png";
 
 const NavigationSidebar = () => {
   const { pathname } = useLocation();
