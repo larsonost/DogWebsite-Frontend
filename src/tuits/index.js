2b3f3905eb4aa7576d1f1d68092218871ba26fdc
Larson Ost <larsonost@outlook.com>
2023-08-15 11:26:52 -0400

    testing backend server connection

--- src/tuits/index.js
+++ src/tuits/index.js
@@ -4,14 +4,26 @@ import { useSelector } from "react-redux";
 import "./waterfall.css";
 
 const TuitsList = () => {
-  const { tuits } = useSelector((state) => state.tuits);
+  const { tuits, loading } = useSelector(state => state.tuits)
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(findTuitsThunk())
+    }, [])
 
   return (
-    <div className="waterfall">
-      {tuits.map((tuit) => (
-        <TuitItem key={tuit._id} tuit={tuit} />
-      ))}
-    </div>
+    <ul className="list-group">
+      {loading &&
+        <li className="list-group-item">
+          Loading...
+        </li>
+      }
+
+      <div className="waterfall">
+        {tuits.map((tuit) => (
+          <TuitItem key={tuit._id} tuit={tuit} />
+        ))}
+      </div>
+    </ul>
   );
 };
 
--- src/tuits/tuit-stats.js
+++ src/tuits/tuit-stats.js
@@ -1,43 +1,77 @@
 import React, { useState } from "react";
-import {AiFillHeart, AiOutlineHeart, AiOutlineUpload} from "react-icons/ai";
-import {FaRetweet} from 'react-icons/fa';
-import {LiaCommentDotsSolid} from "react-icons/lia";
+import { AiFillHeart, AiOutlineHeart, AiOutlineUpload } from "react-icons/ai";
+import { FaRetweet } from 'react-icons/fa';
+import { LiaCommentDotsSolid } from "react-icons/lia";
+import { updateTuitThunk } from "../services/tuits-thunks";
+import { useDispatch } from "react-redux";
+import { FaHeart } from "react-icons/fa";
+import { AiOutlineDislike } from "react-icons/ai";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import { faThumbsDown } from "@fortawesome/free-solid-svg-icons";
 import "./tuit-item.css";
 
 const TuitStats = ({ tuit }) => {
-    const [likes, setLikes] = useState(tuit.likes);
-    const [liked, setLiked] = useState(tuit.liked);
 
-    const handleLikeClick = () => {
+    const dispatch = useDispatch();
+
+
+    const [liked, setLiked] = useState(tuit.liked);
+    const [likesNum, setlikesNum] = useState(tuit.likes);
+
+    const handleLikeToggle = () => {
         if (liked) {
-            setLikes(likes - 1);
-            setLiked(false);
+            setlikesNum((prev) => prev - 1);
+            dispatch(updateTuitThunk({ ...tuit, likes: tuit.likes - 1, liked: !tuit.liked }))
         } else {
-            setLikes(likes + 1);
-            setLiked(true);
+            setlikesNum((prev) => prev + 1);
+            dispatch(updateTuitThunk({ ...tuit, likes: tuit.likes + 1, liked: !tuit.liked }))
         }
+        setLiked((prevLiked) => !prevLiked);
     };
 
-    const likeIcon = liked
-        ? <AiFillHeart color={"red"} onClick={handleLikeClick} />
-        : <AiOutlineHeart onClick={handleLikeClick} />;
+    const [disliked, setdisLiked] = useState(tuit.disliked);
+    const [dislikesNum, setdislikesNum] = useState(tuit.dislikes);
 
+    const handleDisLikeToggle = () => {
+        if (disliked) {
+            setdislikesNum((prev) => prev - 1);
+            dispatch(updateTuitThunk({ ...tuit, dislikes: tuit.dislikes - 1, disliked: !tuit.disliked }))
+        } else {
+            setdislikesNum((prev) => prev + 1);
+            dispatch(updateTuitThunk({ ...tuit, dislikes: tuit.dislikes + 1, disliked: !tuit.disliked }))
+        }
+        setdisLiked((prevdisLiked) => !prevdisLiked);
+    };
 
-    return(
-                 <div className="row alignment">
-                        <div className="col-2">
-                            <LiaCommentDotsSolid/> {tuit.replies}
-                        </div>
-                        <div className="col-2">
-                            <FaRetweet/> {tuit.retuits}
-                        </div>
-                        <div className="col-2">
-                            {likeIcon} {likes}
-                        </div>
-                        <div className="col-2">
-                            <AiOutlineUpload/>
-                        </div>
-                    </div>
+    //portions of the style/icon were given by chatGPT
+    return (
+        <div className="row alignment">
+            <div className="col-2">
+                <LiaCommentDotsSolid /> {tuit.replies}
+            </div>
+            <div className="col-2">
+                <FaRetweet /> {tuit.retuits}
+            </div>
+            <div className="col-3 d-flex align-items-center" onClick={handleLikeToggle}><FontAwesomeIcon
+                icon={faHeart}
+                style={{ color: liked ? "red" : "black" }}
+                className="icon-space"
+            />
+                    {likesNum}
+            </div>
+            <div className="col-3 d-flex align-items-center" onClick={handleDisLikeToggle}><FontAwesomeIcon
+
+                icon={faThumbsDown}
+                style={{ color: disliked ? "blue" : "black" }}
+                className="icon-space"
+            />
+                {dislikesNum}
+            </div>
+            <div className="col-2">
+                <AiOutlineUpload />
+            </div>
+        </div>
     );
 };
 export default TuitStats;
