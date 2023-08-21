b360605a43fbbbd173bfd58c32a9a448f3490e4a
Larson Ost <larsonost@outlook.com>
2023-08-21 15:31:15 -0400

    private info

--- src/profile-screen/index.js
+++ src/profile-screen/index.js
@@ -28,6 +28,9 @@ function ProfileScreen() {
 
     fetchProfile();
   }, [dispatch]);
+  const save = () => {
+    dispatch(updateUserThunk(profile));
+  };
 
   const getValue = (mainObj, backupObj, key) => {
     if (mainObj && mainObj[key] !== undefined) {
@@ -42,6 +45,8 @@ function ProfileScreen() {
   const user = {
     firstName: currentUser ? getValue(currentUser.data, currentUser, 'firstName') : null,
     lastName: currentUser ? getValue(currentUser.data, currentUser, 'lastName') : null,
+    ID: currentUser ? getValue(currentUser.data, currentUser, '_id') : null,
+    password: currentUser ? getValue(currentUser.data, currentUser, 'password') : null,
     username: currentUser ? getValue(currentUser.data, currentUser, 'username') : null,
     role: currentUser ? getValue(currentUser.data, currentUser, 'role') : null,
     dogs: currentUser ? getValue(currentUser.data, currentUser, 'dogs') : [],
@@ -51,7 +56,17 @@ function ProfileScreen() {
     following: currentUser ? getValue(currentUser.data, currentUser, 'following') : [],
 };
 
-
+const renderPrivateDetails = () => (
+  <div className="card mb-4">
+    <div className="card-header">Private Information</div>
+    <div className="card-body">
+      <p><strong>First Name:</strong> {user.firstName}</p>
+      <p><strong>Last Name:</strong> {user.lastName}</p>
+      <p><strong>Website ID:</strong> {user.ID}</p>
+      <p><strong>Password:</strong> {user.password}</p>
+    </div>
+  </div>
+);
   const renderOwnerDetails = () => (
     <div>
       <h5><FontAwesomeIcon icon={faDog} /> Dogs Owned</h5>
@@ -89,46 +104,66 @@ function ProfileScreen() {
 
   return (
     <div className="container">
-      <h2>{user.firstName} {user.lastName}</h2>
-      <h5>@{user.username}</h5>
-      
-      <div className="mb-3">
-        <span><strong>Followers:</strong> {user.followers ? user.followers.length : 0}</span>
-        <span className="ml-4"><strong>Following:</strong> {user.following ? user.following.length : 0}</span>
-      </div>
+        <div className="row">
+            {/* Left column for main content */}
+            <div className="col-md-8">
+                <h2>{user.firstName} {user.lastName}</h2>
+                <h5>@{user.username}</h5>
+                
+                <div className="mb-3">
+                    <span><strong>Followers:</strong> {user.followers ? user.followers.length : 0}</span>
+                    <span className="ml-4"><strong>Following:</strong> {user.following ? user.following.length : 0}</span>
+                </div>
 
-      {user.role === 'Owner' && renderOwnerDetails()}
-      {user.role === 'Merchant' && renderMerchantDetails()}
-      {user.role === 'Specialist' && renderSpecialistDetails()}
+                {user.role === 'Owner' && renderOwnerDetails()}
+                {user.role === 'Merchant' && renderMerchantDetails()}
+                {user.role === 'Specialist' && renderSpecialistDetails()}
 
-      <h4 className="mt-4">Tuits</h4>
-      <div className="waterfall">
-        {relevantTuits.map((tuit) => {
-          switch (tuit.role) {
-            case 'Owner':
-              return <OwnerTuit key={tuit._id} tuit={tuit} />
-            case 'Merchant':
-              return <MerchantTuit key={tuit._id} tuit={tuit} />
-            case 'Specialist':
-              return <SpecialistTuit key={tuit._id} tuit={tuit} />
-            default:
-              return <TuitItem key={tuit._id} tuit={tuit} />
-          }
-        })}
-      </div>
+                <h4 className="mt-4">Tuits</h4>
+                <div className="waterfall">
+                    {relevantTuits.map((tuit) => {
+                        switch (tuit.role) {
+                            case 'Owner':
+                                return <OwnerTuit key={tuit._id} tuit={tuit} />
+                            case 'Merchant':
+                                return <MerchantTuit key={tuit._id} tuit={tuit} />
+                            case 'Specialist':
+                                return <SpecialistTuit key={tuit._id} tuit={tuit} />
+                            default:
+                                return <TuitItem key={tuit._id} tuit={tuit} />
+                        }
+                    })}
+                </div>
 
-      <button className="m-2 btn btn-primary"
-        onClick={() => {
-          dispatch(logoutThunk());
-          navigate("/../login");
-        }}>
-        Logout
-      </button>
-      <button className="m-2 btn btn-primary" onClick={() => dispatch(updateUserThunk(profile))}>
-        Save
-      </button>
+                <button className="m-2 btn btn-primary"
+                    onClick={() => {
+                        dispatch(logoutThunk());
+                        navigate("/../login");
+                    }}>
+                    Logout
+                </button>
+                <button className="m-2 btn btn-primary" onClick={() => dispatch(updateUserThunk(profile))}>
+                    Save
+                </button>
+                <div>
+                    <label>First Name</label>
+                    <input type="text" value={user.firstName}
+                        onChange={(event) => {
+                            const newProfile = {
+                                ...profile, firstName: event.target.value,
+                            };
+                            setProfile(newProfile);
+                        }}/>
+                </div>
+            </div>
+
+            {/* Right column for private details */}
+            <div className="col-md-4">
+                {renderPrivateDetails()}
+            </div>
+        </div>
     </div>
-  );
+);
 }
 
 export default ProfileScreen;
